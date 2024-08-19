699948b57c72710857d845d3e5ae8c86259b5255
Admin <Admin@LAPTOP-JDMOMMYD>
2024-08-19 15:39:15 +0800

    Add project files.

---
+++ usersettings_test/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace usersettings_test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblInput = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.txtSetting = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.chkTxtconfig = new System.Windows.Forms.CheckBox();
+            this.lblDesc = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtList = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblInput
+            // 
+            this.lblInput.AutoSize = true;
+            this.lblInput.Location = new System.Drawing.Point(32, 44);
+            this.lblInput.Name = "lblInput";
+            this.lblInput.Size = new System.Drawing.Size(38, 16);
+            this.lblInput.TabIndex = 0;
+            this.lblInput.Text = "Input:";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(119, 44);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(100, 22);
+            this.txtInput.TabIndex = 1;
+            // 
+            // txtSetting
+            // 
+            this.txtSetting.Enabled = false;
+            this.txtSetting.Location = new System.Drawing.Point(119, 82);
+            this.txtSetting.Name = "txtSetting";
+            this.txtSetting.Size = new System.Drawing.Size(36, 22);
+            this.txtSetting.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Setting:";
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(119, 121);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(59, 22);
+            this.txtOutput.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Output:";
+            // 
+            // chkTxtconfig
+            // 
+            this.chkTxtconfig.AutoSize = true;
+            this.chkTxtconfig.Location = new System.Drawing.Point(35, 204);
+            this.chkTxtconfig.Name = "chkTxtconfig";
+            this.chkTxtconfig.Size = new System.Drawing.Size(186, 20);
+            this.chkTxtconfig.TabIndex = 6;
+            this.chkTxtconfig.Text = "Use Text file User Settings";
+            this.chkTxtconfig.UseVisualStyleBackColor = true;
+            this.chkTxtconfig.CheckedChanged += new System.EventHandler(this.chkTxtconfig_CheckedChanged);
+            // 
+            // lblDesc
+            // 
+            this.lblDesc.AutoSize = true;
+            this.lblDesc.Location = new System.Drawing.Point(161, 85);
+            this.lblDesc.Name = "lblDesc";
+            this.lblDesc.Size = new System.Drawing.Size(73, 16);
+            this.lblDesc.TabIndex = 7;
+            this.lblDesc.Text = "description";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(35, 166);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Check";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtList
+            // 
+            this.txtList.Location = new System.Drawing.Point(35, 230);
+            this.txtList.Multiline = true;
+            this.txtList.Name = "txtList";
+            this.txtList.Size = new System.Drawing.Size(475, 176);
+            this.txtList.TabIndex = 9;
+            this.txtList.Visible = false;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(35, 412);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 10;
+            this.btnSave.Text = "SaveFile";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Visible = false;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(947, 499);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtList);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblDesc);
+            this.Controls.Add(this.chkTxtconfig);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtSetting);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.lblInput);
+            this.Name = "Form1";
+            this.Text = "Zapper Test2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblInput;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.TextBox txtSetting;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox chkTxtconfig;
+        private System.Windows.Forms.Label lblDesc;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtList;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
+
---
+++ usersettings_test/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections.Specialized;
+using System.Threading;
+using System.Security.Policy;
+using System.IO;
+
+namespace usersettings_test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        public void Load_settings_collection()
+        {
+            StringCollection selected_res_string = Properties.Settings.Default.USR_SET;
+            string sText = txtInput.Text;
+
+            for (int i = 0; i < selected_res_string.Count; i++) 
+            { 
+                string iText = selected_res_string[i];
+                string[] stCol;
+                char splitchar = ',';
+                if (iText != null) {
+                    stCol = iText.Split(splitchar);
+                    if (stCol[1].Contains(sText))
+                    {
+                        txtSetting.Text = stCol[2];
+                        txtOutput.Text = stCol[3];
+                        lblDesc.Text = stCol[0];
+                        break;
+                    }
+                    else 
+                    {
+                        txtSetting.Text = null;
+                        txtOutput.Text = null;
+                        lblDesc.Text = null;
+                    }
+                }
+            }
+
+        }
+
+        public void Load_textfile_settings() {
+            string filepath = Application.StartupPath + @"\UserConfig\User_Config.txt";
+            if (File.Exists(filepath))
+            {
+                List<string> lines = new List<string>();
+                lines = File.ReadAllLines(filepath).ToList();
+                string sText = txtInput.Text;
+                for (int i = 0; i < lines.Count; i++)
+                {
+                    string iText = lines[i];
+                    string[] stCol;
+                    char splitchar = ',';
+                    if (iText != null)
+                    {
+                        stCol = iText.Split(splitchar);
+                        if (stCol[1].Contains(sText))
+                        {
+                            txtSetting.Text = stCol[2];
+                            txtOutput.Text = stCol[3];
+                            lblDesc.Text = stCol[0];
+                            File.OpenRead(filepath).Dispose();
+                            break;
+                        }
+                        else
+                        {
+                            txtSetting.Text = null;
+                            txtOutput.Text = null;
+                            lblDesc.Text = null;
+                        }
+                    }
+                }
+            }
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (chkTxtconfig.Checked)
+            {
+                Load_textfile_settings();
+            }
+            else
+            {
+                Load_settings_collection();
+            }
+        }
+
+        private void chkTxtconfig_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkTxtconfig.Checked)
+            {
+                string filepath = Application.StartupPath + @"\UserConfig\User_Config.txt";
+                if (File.Exists(filepath))
+                {
+                    string txtContext = File.ReadAllText(filepath);
+                    txtList.Text = txtContext;
+                    txtList.Visible = true;
+                    btnSave.Visible = true;
+                    File.OpenRead(filepath).Dispose();
+                }
+            }
+            else {
+                txtList.Text = null;
+                txtList.Visible = false;
+                btnSave.Visible = false;
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            string filepath = Application.StartupPath + @"\UserConfig\User_Config.txt";
+            if (File.Exists(filepath)) {
+                if (txtList.Text != null) { 
+                    StreamWriter sw = new StreamWriter(filepath);
+                    sw.Write(txtList.Text);
+                    sw.Close();
+                }
+            }
+
+        }
+    }
+}
